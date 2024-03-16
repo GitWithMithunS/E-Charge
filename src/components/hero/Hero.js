28c50d30e45db87992538f642148a1be6871c6f0
Raghava Reddy N <raghau.20003@gmail.com>
2024-03-17 00:49:55 +0530

    Hero mod

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -18,16 +18,17 @@ const Hero = () => {
                 >
                         <div className="flex justify-center items-center flex-col relative z-10">
                     <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
-                    E-Charge
+                    E-Charge 
                     </motion.h1>
+                    
                     <motion.div
         variants={slideIn('right', 'tween', 0.2, 1)}
         className="relative w-full md:-mt-[20px] -mt-[12px]"
       >
-        <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]" />
+        <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[0px]" />
 
         <img
-          src="/cover.png"
+          src="/cover.jpg"
           alt="hero_cover"
           className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
         />
